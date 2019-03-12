d1885a66c5105c2b4ff7e6c33386d31fa82f9b36
Max Escaler <max@Maxs-MacBook-Pro.local>
2019-03-12 16:58:09 -0400

    Initial Commit

---
+++ proj1.py
@@ -0,0 +1,13 @@
+import sys
+
+seed = argv[1]
+lmda = argv[2]
+upperBound = argv[3]
+numProcesses = argv[4]
+tcs = argv[5]
+alpha = argv[6]
+timeSlice = argv[7]
+if(argv[8].equals("BEGINNING"):
+	rrAdd = 1
+else:
+	rrAdd = 0
