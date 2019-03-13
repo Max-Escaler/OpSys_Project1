8bc7e476e878b86a34b68d1021e14aedeedbe857
Max Escaler <max@Maxs-MacBook-Pro.local>
2019-03-13 14:30:45 -0400

    Added pseudorandom generation

--- proj1.py
+++ proj1.py
@@ -1,14 +1,122 @@
 import sys
+import math
 
-seed = argv[1]
-lmda = argv[2]
-upperBound = argv[3]
-numProcesses = argv[4]
-tcs = argv[5]
-alpha = argv[6]
-timeSlice = argv[7]
-if(argv[8].equals("BEGINNING")):
-	rrAdd = 1
-else:
-	rrAdd = 0
+seed = int(sys.argv[1])
+lmda = float(sys.argv[2])
+upperBound = int(sys.argv[3])
+numProcesses = int(sys.argv[4])
+tcs = sys.argv[5]
+alpha = sys.argv[6]
+timeSlice = sys.argv[7]
+
+
+class Rand48(object):
+    def __init__(self, seed):
+        self.n = seed
+    def seed(self, seed):
+        self.n = seed
+    def srand(self, seed):
+        self.n = (seed << 16) + 0x330e
+    def next(self):
+        self.n = (25214903917 * self.n + 11) & (2**48 - 1)
+        return self.n
+    def drand(self):
+        return self.next() / 2**48
+    def lrand(self):
+        return self.next() >> 17
+    def mrand(self):
+        n = self.next() >> 16
+        if n & (1 << 31):
+            n -= 1 << 32
+        return n 
+
+
+
+class Process:
+	arrivalTime = 0
+	bursts = 0
+	burstTimes = []
+	IOTimes= []
+	state = "READY"
+	def __init__(self,at, b,bt,iot):
+		self.arrivalTime = at
+		self.bursts = b
+		self.burstTimes = bt
+		self.IOTimes = iot
+
+	def getAT(self):
+		return self.arrivalTime
+
+	def getCPUBursts(self):
+		return self.burstTimes
+	def getIOBursts(self):
+		return self.IOTimes
+
+## Driver Function
+
+rand = Rand48(seed)
+rand.srand(seed)
+procceses = []
+for x in range(numProcesses):
+	y=upperBound+1
+	while (y>upperBound):
+		r = rand.drand()
+		y = -math.log(r)/lmda;
+		
+	arrivalTime =  math.floor(y)
+	bursts = int(rand.drand() * 100)+1
+	cpuBurst = []
+	ioBurst = []
+	for q in range(bursts):
+		if (q == bursts-1):
+			p=upperBound+1
+			while (p>upperBound):
+				r = rand.drand()
+				p = -math.log(r)/lmda;
+
+
+			cpuBurst.append(math.ceil(p))
+			break
+
+
+		p=upperBound+1
+		while (p>upperBound):
+			r = rand.drand()
+			p = -math.log(r)/lmda;
+
+
+		cpuBurst.append(math.ceil(p))
+
+		p=upperBound+1
+		while (p>upperBound):
+			r = rand.drand()
+			p = -math.log(r)/lmda;
+		ioBurst.append(math.ceil(p))
+
+	z = Process(arrivalTime,bursts,cpuBurst,ioBurst)
+	procceses.append(z)
+
+
+for y in procceses:
+	print("Arrival time:", y.getAT())
+	print("CPUBurst Times:",len(y.getCPUBursts()))
+	print("IOBurst  Times:",len(y.getIOBursts()))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ test.py
@@ -0,0 +1,9 @@
+def one(x):
+	x.append(1)
+	x = [2,3]
+
+
+
+y = [0]
+one(y)
+print(y)
